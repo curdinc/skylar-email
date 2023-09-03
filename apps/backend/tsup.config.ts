2cc435865e95c79f037f3d91886457f6d8f7f208
ElasticBottle <winstonyeo99@yahoo.com>
2023-09-02 21:21:05 -0700

    ci(backend): update gh action scripts

--- apps/backend/tsup.config.ts
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from "tsup";
-
-export default defineConfig({
-  entry: ["./src/index.ts"],
-  noExternal: ["@hono/trpc-server", "hono", "@skylar/api"],
-  sourcemap: true,
-});
