3702a11754a4ef89ab6392baa303b32af81e6c14
Hans Bhatia <h23bhati@uwaterloo.ca>
2023-09-05 01:36:20 -0700

    current-backend

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -5,7 +5,6 @@ import { env } from "hono/adapter";
 import { cors } from "hono/cors";
 
 import { appRouter, createTRPCContext } from "@skylar/api";
-import { honoAuthMiddleware } from "@skylar/auth";
 import { getDb } from "@skylar/db";
 import { getServerLogger } from "@skylar/logger";
 import { BackendEnvSchema, formatValidatorError, parse } from "@skylar/schema";
@@ -34,6 +33,15 @@ function getEnvVars(
   }
 }
 
+app.options("/trpc/*", (c) => {
+  const response = c.newResponse(null, { status: 204 });
+  response.headers.set("Access-Control-Allow-Origin", "*");
+  response.headers.set("Access-Control-Request-Method", "*");
+  response.headers.set("Access-Control-Allow-Methods", "OPTIONS, GET, POST");
+  response.headers.set("Access-Control-Allow-Headers", "*");
+  return response;
+});
+
 app.use("/trpc/*", async (c, next) => {
   const envVars = getEnvVars(c);
   return await cors({
@@ -44,15 +52,7 @@ app.use("/trpc/*", async (c, next) => {
 
 app.use("/trpc/*", async (c, next) => {
   const envVars = getEnvVars(c);
-
-  return await honoAuthMiddleware({
-    SUPABASE_ANON_KEY: envVars.SUPABASE_ANON_KEY,
-    SUPABASE_URL: envVars.SUPABASE_URL,
-  })(c, next);
-});
-
-app.use("/trpc/*", async (c, next) => {
-  const envVars = getEnvVars(c);
+  console.log(JSON.stringify(c.req.headers.get("Content-Type"), null, 2));
 
   const db = getDb(envVars.DATABASE_URL);
   const logger = getServerLogger({
@@ -62,32 +62,37 @@ app.use("/trpc/*", async (c, next) => {
     orgId: envVars.AXIOM_ORG_ID,
     url: envVars.AXIOM_URL,
   });
+  // console.log("await c.req.raw.text()", await c.req.raw.text());
 
   const response = await trpcServer({
     router: appRouter,
     endpoint: "/trpc",
     onError({ error, path }) {
-      console.error(`>>> tRPC Error on '${path}'`, error);
+      console.error(
+        `>>> tRPC Error on '${path}'`,
+        error,
+        JSON.stringify(formatValidatorError(error.cause), null, 2),
+      );
     },
-    createContext: ({ req }) =>
-      createTRPCContext({
-        req,
-        env: { JWT_SECRET: envVars.SUPABASE_JWT_SECRET },
+    createContext: () => {
+      // console.log(
+      //   "trpc_context_req",
+      //   JSON.stringify(await req.json(), null, 2),
+      // );
+      return createTRPCContext({
+        req: c.req.raw,
+        env: {
+          JWT_SECRET: envVars.SUPABASE_JWT_SECRET,
+          GOOGLE_PROVIDER_CLIENT_ID: envVars.GOOGLE_PROVIDER_CLIENT_ID,
+          GOOGLE_PROVIDER_CLIENT_SECRET: envVars.GOOGLE_PROVIDER_CLIENT_SECRET,
+        },
         db,
         logger,
-      }),
+      });
+    },
   })(c, next);
 
   await logger.flush();
-  return response;
-});
-
-app.options("/trpc/*", (c) => {
-  const response = c.newResponse(null, { status: 204 });
-  response.headers.set("Access-Control-Allow-Origin", "*");
-  response.headers.set("Access-Control-Request-Method", "*");
-  response.headers.set("Access-Control-Allow-Methods", "OPTIONS, GET, POST");
-  response.headers.set("Access-Control-Allow-Headers", "*");
   return response;
 });
 
