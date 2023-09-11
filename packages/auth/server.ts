061a99d967479c6c7b4ef373b3f0dcfa1e52f7da
ElasticBottle <winstonyeo99@yahoo.com>
2023-09-11 11:35:03 -0500

    chore(auth): export auth setting types

--- packages/auth/server.ts
+++ packages/auth/server.ts
@@ -2,4 +2,8 @@ export { cookieOptions } from "./src/constants";
 export { getSession } from "./src/server";
 export { honoAuthMiddleware } from "./src/server/hono";
 export { NextAuthProvider } from "./src/server/next-provider";
+export type {
+  AuthSettingServerType,
+  PathType,
+} from "./src/types/auth-settings";
 export type { Session } from "./src/types/session";
