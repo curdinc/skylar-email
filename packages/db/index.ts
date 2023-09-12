79a11a4567f0f4171ff0e735435684d6aadb8b96
ElasticBottle <winstonyeo99@yahoo.com>
2023-09-12 02:27:27 -0500

    chore(db): update schema files

--- packages/db/index.ts
+++ packages/db/index.ts
@@ -1,11 +1,10 @@
 import { neon, neonConfig } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-http";
 
-import * as example from "./schema/example";
 import { inviteCode } from "./schema/invite-code";
 import { user } from "./schema/user";
 
-export const schema = { ...example, user, inviteCode };
+export const schema = { user, inviteCode };
 
 export function getDb(dbConnectionString: string) {
   neonConfig.fetchConnectionCache = true;
--- packages/db/migrations/0002_high_harrier.sql
+++
@@ -1,7 +0,0 @@
-ALTER TABLE "inviteCode" DROP CONSTRAINT "inviteCode_userId_user_id_fk";
---> statement-breakpoint
-DO $$ BEGIN
- ALTER TABLE "inviteCode" ADD CONSTRAINT "inviteCode_userId_user_id_fk" FOREIGN KEY ("userId") REFERENCES "user"("id") ON DELETE set null ON UPDATE cascade;
-EXCEPTION
- WHEN duplicate_object THEN null;
-END $$;
