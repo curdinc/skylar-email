cb1d67eaaf82c980205687729cb29bfab4b8b268
Hans Bhatia <h23bhati@uwaterloo.ca>
2023-09-14 22:54:32 -0700

    chore: export drizzle functions

--- packages/db/index.ts
+++ packages/db/index.ts
@@ -4,6 +4,8 @@ import { drizzle } from "drizzle-orm/neon-http";
 import * as providerAuthDetails from "./schema/emailProviders";
 import * as users from "./schema/users";
 
+export { and, eq } from "drizzle-orm";
+
 export const schema = { ...providerAuthDetails, ...users };
 
 export function getDb(dbConnectionString: string) {
