404b4f8a8df2aa6ff475e64b3bc518ce151683c5
ElasticBottle <winstonyeo99@yahoo.com>
2023-09-17 02:25:11 -0700

    feat(nextjs): Add description to onboarding card page

--- apps/nextjs/src/app/(emailClient)/onboarding/card/payment-collection-form.tsx
+++ apps/nextjs/src/app/(emailClient)/onboarding/card/payment-collection-form.tsx
@@ -3,6 +3,7 @@
 import { PaymentElement } from "@stripe/react-stripe-js";
 
 import { Button } from "~/components/ui/button";
+import { CardDescription, CardHeader, CardTitle } from "~/components/ui/card";
 import { useCardPage } from "./use-card-page";
 
 export const PaymentCollectionForm = () => {
@@ -14,19 +15,29 @@ export const PaymentCollectionForm = () => {
   } = useCardPage();
 
   return (
-    <form
-      className="flex min-h-[413px] flex-col space-y-5"
-      onSubmit={handlePaymentElementSubmit}
-    >
-      <div className="grow">
-        <PaymentElement onReady={onPaymentElementLoad} />
-      </div>
-      <Button
-        disabled={!canSubmitPaymentDetail}
-        isLoading={isSubmittingPaymentDetails}
+    <div className="flex flex-col space-y-6 pt-5">
+      <CardHeader className="p-0">
+        <CardTitle>Card Details</CardTitle>
+        <CardDescription>
+          To prevent spam and enable seamless upgrades, we require a card on
+          file. We will not charge you before explicitly asking for your
+          consent.
+        </CardDescription>
+      </CardHeader>
+      <form
+        className="flex min-h-[413px] flex-col space-y-5"
+        onSubmit={handlePaymentElementSubmit}
       >
-        Submit
-      </Button>
-    </form>
+        <div className="grow">
+          <PaymentElement onReady={onPaymentElementLoad} />
+        </div>
+        <Button
+          disabled={!canSubmitPaymentDetail}
+          isLoading={isSubmittingPaymentDetails}
+        >
+          Submit
+        </Button>
+      </form>
+    </div>
   );
 };
