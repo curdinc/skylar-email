da3c8054175af0548901490f869c053c90c7067a
ElasticBottle <winstonyeo99@yahoo.com>
2023-09-22 02:46:39 -0700

    feat(logic): update logic to include client db information chore(nextjs): add client-db package

--- packages/logic/index.ts
+++ packages/logic/index.ts
@@ -1,5 +1,18 @@
 import { enableReactUse } from "@legendapp/state/config/enableReactUse";
+import {
+  configureObservablePersistence,
+  persistObservable,
+} from "@legendapp/state/persist";
+import { ObservablePersistLocalStorage } from "@legendapp/state/persist-plugins/local-storage";
+
+import { state$ } from "./src";
 
 enableReactUse();
+configureObservablePersistence({
+  pluginLocal: ObservablePersistLocalStorage,
+});
+persistObservable(state$.EMAIL_CLIENT, {
+  local: "email_client", // Unique name
+});
 
 export * from "./src";
