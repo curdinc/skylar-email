04a22f3bfe7a251d00c268651328da046c424f1c
ElasticBottle <winstonyeo99@yahoo.com>
2023-09-24 01:02:41 -0700

    refactor(gmail-api): allow gmail sync to throw so that errors can be parsed in front-end

--- packages/gmail-api/src/routines/full-sync.ts
+++ packages/gmail-api/src/routines/full-sync.ts
@@ -13,38 +13,20 @@ export async function fullSync({
   emailId: string;
   logger: Logger;
 }): Promise<SyncResponseType> {
-  try {
-    // get all messages
-    const messages = await getMessageListUnbounded({
-      accessToken,
-      emailId,
-    });
-    const messageIds = messages.map((m) => m.id);
+  // get all messages
+  const messages = await getMessageListUnbounded({
+    accessToken,
+    emailId,
+  });
+  const messageIds = messages.map((m) => m.id);
 
-    const newMessages = await getAndParseMessages({
-      accessToken: accessToken,
-      emailId,
-      messageIds,
-      logger,
-    });
-    return {
-      newMessages,
-    };
-  } catch (e) {
-    if (e instanceof Error) {
-      logger.error("Error in partial sync.", {
-        e,
-        cause: "known",
-        emailId,
-      });
-    } else {
-      logger.error("Error in partial sync.", {
-        cause: "unknown",
-        emailId,
-      });
-    }
-    return {
-      newMessages: [],
-    };
-  }
+  const newMessages = await getAndParseMessages({
+    accessToken: accessToken,
+    emailId,
+    messageIds,
+    logger,
+  });
+  return {
+    newMessages,
+  };
 }
--- packages/gmail-api/src/routines/partial-sync.ts
+++ packages/gmail-api/src/routines/partial-sync.ts
@@ -15,52 +15,30 @@ export async function partialSync({
   startHistoryId: string;
   logger: Logger;
 }): Promise<SyncResponseType> {
-  try {
-    // get all messages
-    const messageChanges = await getMessageChangesFromHistoryId({
-      accessToken,
-      emailId,
-      startHistoryId,
-    });
+  // get all messages
+  const messageChanges = await getMessageChangesFromHistoryId({
+    accessToken,
+    emailId,
+    startHistoryId,
+  });
 
-    if (messageChanges.messagesAdded.length === 0) {
-      return {
-        newMessages: [],
-        messagesDeleted: messageChanges.messagesDeleted,
-        labelsModified: messageChanges.labelsModified,
-      };
-    }
-
-    const newMessages = await getAndParseMessages({
-      accessToken: accessToken,
-      emailId,
-      messageIds: messageChanges.messagesAdded,
-      logger,
-    });
+  if (messageChanges.messagesAdded.length === 0) {
     return {
-      newMessages,
+      newMessages: [],
       messagesDeleted: messageChanges.messagesDeleted,
       labelsModified: messageChanges.labelsModified,
     };
-  } catch (e) {
-    if (e instanceof Error) {
-      logger.error("Error in partial sync.", {
-        e,
-        cause: "known",
-        emailId,
-        startHistoryId,
-      });
-    } else {
-      logger.error("Error in partial sync.", {
-        cause: "unknown",
-        emailId,
-        startHistoryId,
-      });
-    }
-    return {
-      newMessages: [],
-      messagesDeleted: [],
-      labelsModified: [],
-    };
   }
+
+  const newMessages = await getAndParseMessages({
+    accessToken: accessToken,
+    emailId,
+    messageIds: messageChanges.messagesAdded,
+    logger,
+  });
+  return {
+    newMessages,
+    messagesDeleted: messageChanges.messagesDeleted,
+    labelsModified: messageChanges.labelsModified,
+  };
 }
