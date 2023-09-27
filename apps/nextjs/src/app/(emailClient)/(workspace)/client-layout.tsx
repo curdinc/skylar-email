23a4aefcb2e70030766a87761759f379d76e1760
ElasticBottle <winstonyeo99@yahoo.com>
2023-09-26 23:41:55 -0700

    feat(nextjs): add partial sync stub

--- apps/nextjs/src/app/(emailClient)/(workspace)/client-layout.tsx
+++ apps/nextjs/src/app/(emailClient)/(workspace)/client-layout.tsx
@@ -2,14 +2,21 @@
 
 import { useEffect } from "react";
 import { useRouter } from "next/navigation";
+import { useQuery } from "@tanstack/react-query";
 
 import { useEmailSyncInfo } from "@skylar/client-db";
-import { state$, useActiveEmailClientDb } from "@skylar/logic";
+import {
+  state$,
+  useActiveEmailClientDb,
+  useActiveEmailProvider,
+} from "@skylar/logic";
 
 import { api } from "~/lib/api";
+import { useEmailPartialSync } from "./use-email-partial-sync";
 
 export const ClientLayout = () => {
   const activeClientDb = useActiveEmailClientDb();
+  const activeEmailClient = useActiveEmailProvider();
   const router = useRouter();
   const { data: emailProviders, isLoading: isLoadingEmailProviders } =
     api.emailProvider.getAll.useQuery();
@@ -17,6 +24,33 @@ export const ClientLayout = () => {
   const { emailSyncInfo, isLoading: isLoadingEmailSyncInfo } = useEmailSyncInfo(
     { db: activeClientDb },
   );
+  const { startEmailPartialSync } = useEmailPartialSync();
+
+  useQuery({
+    queryKey: [
+      isLoadingEmailSyncInfo,
+      emailSyncInfo?.last_sync_history_id,
+      activeEmailClient?.email,
+    ],
+    queryFn: async () => {
+      if (isLoadingEmailSyncInfo || !activeEmailClient?.email) {
+        return false;
+      }
+      if (!emailSyncInfo?.last_sync_history_id) {
+        router.push("/onboarding/sync");
+        return false;
+      }
+      const emailData = await startEmailPartialSync(
+        activeEmailClient?.email,
+        emailSyncInfo?.last_sync_history_id,
+      );
+      console.log("emailData", emailData);
+      return true;
+    },
+    refetchInterval: 50_000, // 50 seconds in milliseconds
+    refetchIntervalInBackground: true,
+    enabled: !isLoadingEmailSyncInfo && !!activeEmailClient?.email,
+  });
 
   useEffect(() => {
     if (isLoadingEmailProviders) {
@@ -26,12 +60,6 @@ export const ClientLayout = () => {
       state$.EMAIL_CLIENT.initializeClientDbs();
     }
   }, [emailProviders, isLoadingEmailProviders]);
-
-  useEffect(() => {
-    if (!isLoadingEmailSyncInfo && !emailSyncInfo) {
-      router.push("/onboarding/sync");
-    }
-  }, [emailSyncInfo, isLoadingEmailSyncInfo, router]);
 
   return <></>;
 };
