6c0a88c6000ad29bd9d9188ac3ea8aa3142bf706
ElasticBottle <winstonyeo99@yahoo.com>
2023-09-28 02:18:37 -0700

    chore(nextjs): Update hover styles and change to link

--- apps/nextjs/src/app/(emailClient)/(workspace)/inbox/page.tsx
+++ apps/nextjs/src/app/(emailClient)/(workspace)/inbox/page.tsx
@@ -1,7 +1,10 @@
 "use client";
 
-import { Button } from "~/components/ui/button";
+import Link from "next/link";
+
+import { Button, buttonVariants } from "~/components/ui/button";
 import { RawHtmlDisplay } from "~/components/ui/raw-html-display";
+import { cn } from "~/lib/ui";
 import { useInboxPage } from "./use-inbox-page";
 
 export default function ImportantInbox() {
@@ -12,12 +15,18 @@ export default function ImportantInbox() {
   }
   const ThreadList = threads?.map((thread) => {
     return (
-      <div
+      <Link
+        href={`/inbox/${thread.email_provider_thread_id}`}
         key={thread.email_provider_thread_id}
-        className="grid grid-cols-1 gap-1"
+        className={cn(
+          buttonVariants({
+            variant: "ghost",
+          }),
+          "grid h-16 grid-cols-1 gap-1 px-1 py-1 md:px-2",
+        )}
       >
         <div className="flex w-full flex-col md:flex-row md:items-baseline md:gap-2">
-          <div className="min-w-0 truncate font-semibold md:text-lg">
+          <div className="min-w-0 truncate text-base font-semibold md:text-lg">
             {thread.subject}
           </div>
           <div className="min-w-fit text-xs text-muted-foreground md:text-sm">
@@ -28,11 +37,11 @@ export default function ImportantInbox() {
           className="truncate text-sm text-muted-foreground"
           html={thread.latest_snippet_html}
         />
-      </div>
+      </Link>
     );
   });
   return (
-    <div className="grid grid-cols-1 gap-6">
+    <div className="grid grid-cols-1 gap-5 p-1 md:gap-6">
       {ThreadList}
       <div className="flex justify-between">
         <Button variant={"secondary"} onClick={prevPage}>
