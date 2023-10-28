601f45f11d52291e453c51e2d39c257415624eff
HansBhatia <h23bhati@uwaterloo.ca>
2023-10-28 22:01:51 +0400

    fix(nextjs): replace invalid get clientdb hook

--- apps/nextjs/src/components/gmail-inbox/inbox-tools.tsx
+++ apps/nextjs/src/components/gmail-inbox/inbox-tools.tsx
@@ -7,7 +7,7 @@ import {
   TooltipTrigger,
 } from "@radix-ui/react-tooltip";
 
-import { useActiveEmailClientDb } from "@skylar/logic";
+import { clientDb } from "@skylar/client-db";
 
 import { api } from "~/lib/api";
 import { Button } from "../ui/button";
@@ -21,89 +21,86 @@ function InboxTools({
   activeEmail: string;
   hoverOptions: hoverOption[];
 }) {
-  const activeClientDb = useActiveEmailClientDb();
   const { toast, dismiss } = useToast();
   const { mutateAsync: fetchGmailAccessToken } =
     api.gmail.getAccessToken.useMutation();
   const [menuDisable, setmenuDisable] = useState(false);
 
-  if (activeClientDb) {
-    const showUndoSuccessToast = () => {
-      toast({
-        title: "Action Undone",
-        duration: 10000,
-        description: "Operation successfull!",
-      });
-    };
+  const showUndoSuccessToast = () => {
+    toast({
+      title: "Action Undone",
+      duration: 10000,
+      description: "Operation successfull!",
+    });
+  };
 
-    const showUndoToast = (item: hoverOption) => {
-      toast({
-        title: item.undoToastConfig.title,
-        duration: 10000,
-        description: "Operation successfull!",
-        action: (
-          <ToastAction
-            onClick={async (e) => {
-              e.preventDefault();
-              const accessToken = await fetchGmailAccessToken({
-                email: activeEmail,
-              });
-              await item.undoFn(accessToken, activeClientDb);
-              dismiss();
-              showUndoSuccessToast();
-            }}
-            altText="Undo"
-          >
-            Undo
-          </ToastAction>
-        ),
-      });
-    };
+  const showUndoToast = (item: hoverOption) => {
+    toast({
+      title: item.undoToastConfig.title,
+      duration: 10000,
+      description: "Operation successfull!",
+      action: (
+        <ToastAction
+          onClick={async (e) => {
+            e.preventDefault();
+            const accessToken = await fetchGmailAccessToken({
+              email: activeEmail,
+            });
+            await item.undoFn(accessToken, clientDb);
+            dismiss();
+            showUndoSuccessToast();
+          }}
+          altText="Undo"
+        >
+          Undo
+        </ToastAction>
+      ),
+    });
+  };
 
-    return (
-      <>
-        {hoverOptions.map((item, ind) => (
-          <div
-            className="group/archive flex flex-col items-center gap-2 group-hover:visible lg:invisible"
-            key={ind}
-          >
-            <TooltipProvider>
-              <Tooltip>
-                <TooltipTrigger>
-                  <Button
-                    className="rounded-full p-1 hover:bg-slate-200"
-                    variant={"ghost"}
-                    size={"icon-md"}
-                    disabled={menuDisable}
-                    onClick={async (e) => {
-                      e.preventDefault();
-                      setmenuDisable(true);
-                      const accessToken = await fetchGmailAccessToken({
-                        email: activeEmail,
-                      });
-                      item
-                        .applyFn(accessToken, activeClientDb)
-                        .then((_) => {
-                          showUndoToast(item);
-                        })
-                        .catch((e) => console.error(e));
-                    }}
-                  >
-                    {item.icon}
-                  </Button>
-                </TooltipTrigger>
-                <TooltipContent>
-                  <div className="invisible mb-2 rounded-md bg-black p-2 text-white group-hover/archive:visible">
-                    {item.tooltipDescription}
-                  </div>
-                </TooltipContent>
-              </Tooltip>
-            </TooltipProvider>
-          </div>
-        ))}
-      </>
-    );
-  }
+  return (
+    <>
+      {hoverOptions.map((item, ind) => (
+        <div
+          className="group/archive flex flex-col items-center gap-2 group-hover:visible lg:invisible"
+          key={ind}
+        >
+          <TooltipProvider>
+            <Tooltip>
+              <TooltipTrigger>
+                <Button
+                  className="rounded-full p-1 hover:bg-slate-200"
+                  variant={"ghost"}
+                  size={"icon-md"}
+                  disabled={menuDisable}
+                  onClick={async (e) => {
+                    e.preventDefault();
+                    setmenuDisable(true);
+                    const accessToken = await fetchGmailAccessToken({
+                      email: activeEmail,
+                    });
+                    item
+                      .applyFn(accessToken, clientDb)
+                      .then((_) => {
+                        showUndoToast(item);
+                      })
+                      .catch((e) => console.error(e));
+                  }}
+                >
+                  {item.icon}
+                </Button>
+              </TooltipTrigger>
+              <TooltipContent>
+                <div className="invisible mb-2 rounded-md bg-black p-2 text-white group-hover/archive:visible">
+                  {item.tooltipDescription}
+                </div>
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
+        </div>
+      ))}
+    </>
+  );
 }
 
 export default InboxTools;
