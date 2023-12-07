a0e4d1e05c41a4cadf296f1a3b92afb70e8b962f
HansBhatia <h23bhati@uwaterloo.ca>
2023-12-07 15:50:47 +0000

    fix(api): remove db dep

--- packages/api/src/router/oauth.ts
+++ packages/api/src/router/oauth.ts
@@ -1,7 +1,6 @@
 import { TRPCError } from "@trpc/server";
 import jwt from "@tsndr/cloudflare-worker-jwt";
 
-import { insertEmailProviderDetail, insertGmailProvider } from "@skylar/db";
 import { getAccessToken } from "@skylar/gmail-api";
 import {
   gmailProviderIDTokenSchema,
@@ -16,89 +15,39 @@ import { protectedProcedure } from "../trpc/procedures";
 export const oauthRouter = createTRPCRouter({
   googleCodeExchange: protectedProcedure
     .input(validatorTrpcWrapper(oauthOnboardingSchema))
-    .mutation(
-      async ({
-        ctx: {
-          env,
-          db,
-          session: { user },
+    .mutation(async ({ ctx: { env }, input }) => {
+      if (input.provider !== "gmail") {
+        throw new TRPCError({
+          code: "NOT_IMPLEMENTED",
+          message: "Only gmail is currently supported.",
+        });
+      }
+      const parsedResponse = await getAccessToken({
+        clientId: env.GOOGLE_PROVIDER_CLIENT_ID,
+        clientSecret: env.GOOGLE_PROVIDER_CLIENT_SECRET,
+        authorizationCode: input.code,
+        grantType: "authorization_code",
+      });
+
+      const { payload } = jwt.decode(parsedResponse.id_token);
+
+      const { email, name, picture } = parse(
+        gmailProviderIDTokenSchema,
+        payload,
+      );
+
+      return {
+        idToken: parsedResponse.id_token,
+        accessToken: parsedResponse.access_token,
+        tokenType: parsedResponse.token_type,
+        scope: parsedResponse.scope,
+        providerType: input.provider,
+        providerInfo: {
+          email,
+          name,
+          imageUri: picture ?? "",
+          refreshToken: parsedResponse.refresh_token,
         },
-        input,
-      }) => {
-        if (input.provider !== "gmail") {
-          throw new TRPCError({
-            code: "NOT_IMPLEMENTED",
-            message: "Only gmail is currently supported.",
-          });
-        }
-        const parsedResponse = await getAccessToken({
-          clientId: env.GOOGLE_PROVIDER_CLIENT_ID,
-          clientSecret: env.GOOGLE_PROVIDER_CLIENT_SECRET,
-          authorizationCode: input.code,
-          grantType: "authorization_code",
-        });
-
-        const { payload } = jwt.decode(parsedResponse.id_token);
-
-        const { email, name, picture } = parse(
-          gmailProviderIDTokenSchema,
-          payload,
-        );
-
-        const emailProviderDetail = await insertEmailProviderDetail({
-          db,
-          input: {
-            email,
-            emailProvider: input.provider,
-            inboxName: name,
-            userId: user.userId,
-            imageUri: picture,
-          },
-        });
-        if (!emailProviderDetail) {
-          throw new TRPCError({
-            code: "INTERNAL_SERVER_ERROR",
-            message: "Failed to insert email provider detail.",
-          });
-        }
-
-        const gmailProvider = await insertGmailProvider({
-          db,
-          input: {
-            emailProviderDetailId: emailProviderDetail?.emailProviderDetailId,
-            // TODO: encrypt refresh token
-            refreshToken: parsedResponse.refresh_token,
-          },
-        });
-
-        if (!gmailProvider) {
-          throw new TRPCError({
-            code: "INTERNAL_SERVER_ERROR",
-            message: "Failed to insert gmail provider.",
-          });
-        }
-
-        if (!email) {
-          throw new TRPCError({
-            code: "INTERNAL_SERVER_ERROR",
-            message: "Failed to get email from id token.",
-          });
-        }
-
-        return {
-          emailProviderDetail,
-          idToken: parsedResponse.id_token,
-          accessToken: parsedResponse.access_token,
-          tokenType: parsedResponse.token_type,
-          scope: parsedResponse.scope,
-          providerType: input.provider,
-          providerInfo: {
-            email,
-            name,
-            imageUri: user.imageUri ?? "",
-            refreshToken: parsedResponse.refresh_token,
-          },
-        };
-      },
-    ),
+      };
+    }),
 });
