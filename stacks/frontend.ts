2d9c720c9e88a8f7607aee7b6f0e34fdd548b9f8
ElasticBottle <winstonyeo99@yahoo.com>
2023-12-11 02:06:21 +0000

    ci: bind env vars to next app

--- stacks/frontend.ts
+++ stacks/frontend.ts
@@ -3,6 +3,13 @@ import { NextjsSite, StackContext } from "sst/constructs";
 export function Frontend({ stack }: StackContext) {
   const site = new NextjsSite(stack, "frontend", {
     path: "./apps/frontend",
+    runtime: "nodejs20.x",
+    environment: {
+      NEXT_PUBLIC_BACKEND_URL: process.env.NEXT_PUBLIC_BACKEND_URL,
+      NEXT_PUBLIC_GOOGLE_PROVIDER_CLIENT_ID:
+        process.env.NEXT_PUBLIC_GOOGLE_PROVIDER_CLIENT_ID,
+      NEXT_PUBLIC_POSTHOG_KEY: process.env.NEXT_PUBLIC_POSTHOG_KEY,
+    },
   });
 
   stack.addOutputs({
