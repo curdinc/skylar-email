1699fb2d8ff4f56cb329a10417a7e95f41fb60f6
ElasticBottle <winstonyeo99@yahoo.com>
2023-12-11 02:31:18 +0000

    ci: clean up stacks code

--- stacks/frontend.ts
+++ stacks/frontend.ts
@@ -5,10 +5,10 @@ export function Frontend({ stack }: StackContext) {
     path: "./apps/frontend",
     runtime: "nodejs20.x",
     environment: {
-      NEXT_PUBLIC_BACKEND_URL: process.env.NEXT_PUBLIC_BACKEND_URL,
+      NEXT_PUBLIC_BACKEND_URL: process.env.NEXT_PUBLIC_BACKEND_URL!,
       NEXT_PUBLIC_GOOGLE_PROVIDER_CLIENT_ID:
-        process.env.NEXT_PUBLIC_GOOGLE_PROVIDER_CLIENT_ID,
-      NEXT_PUBLIC_POSTHOG_KEY: process.env.NEXT_PUBLIC_POSTHOG_KEY,
+        process.env.NEXT_PUBLIC_GOOGLE_PROVIDER_CLIENT_ID!
+      NEXT_PUBLIC_POSTHOG_KEY: process.env.NEXT_PUBLIC_POSTHOG_KEY!,
     },
   });
 
