3519fe2d8f2b1654526c391e293f661c6bfabc2f
ElasticBottle <winstonyeo99@yahoo.com>
2023-12-11 02:33:07 +0000

    ci: update stacks code

--- stacks/frontend.ts
+++ stacks/frontend.ts
@@ -7,7 +7,7 @@ export function Frontend({ stack }: StackContext) {
     environment: {
       NEXT_PUBLIC_BACKEND_URL: process.env.NEXT_PUBLIC_BACKEND_URL!,
       NEXT_PUBLIC_GOOGLE_PROVIDER_CLIENT_ID:
-        process.env.NEXT_PUBLIC_GOOGLE_PROVIDER_CLIENT_ID!
+        process.env.NEXT_PUBLIC_GOOGLE_PROVIDER_CLIENT_ID!,
       NEXT_PUBLIC_POSTHOG_KEY: process.env.NEXT_PUBLIC_POSTHOG_KEY!,
     },
   });
