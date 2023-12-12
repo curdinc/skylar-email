911ba0c18573e85794b36685a80998118c9705bb
ElasticBottle <winstonyeo99@yahoo.com>
2023-12-12 03:47:26 +0000

    build: extract common env vars build: add custom domain

--- stacks/skylar-email.ts
+++ stacks/skylar-email.ts
@@ -1,23 +1,35 @@
 import { Api, NextjsSite, StackContext } from "sst/constructs";
 
+const BASE_CUSTOM_DOMAIN = "curdinc.com";
+
 export function SkylarEmailConstructs({ stack }: StackContext) {
+  stack.addDefaultFunctionEnv({
+    NEXT_PUBLIC_AXIOM_TOKEN: process.env.NEXT_PUBLIC_AXIOM_TOKEN!,
+    NEXT_PUBLIC_AXIOM_DATASET: process.env.NEXT_PUBLIC_AXIOM_DATASET!,
+    NEXT_PUBLIC_GOOGLE_PROVIDER_CLIENT_ID:
+      process.env.NEXT_PUBLIC_GOOGLE_PROVIDER_CLIENT_ID!,
+  });
+
   const backend = new Api(stack, "Api", {
     defaults: {
       function: {
         runtime: "nodejs20.x",
         environment: {
-          FRONTEND_URL: process.env.FRONTEND_URL!,
+          FRONTEND_URL: `${stack.stage}.${BASE_CUSTOM_DOMAIN}`,
           DATABASE_URL: process.env.DATABASE_URL!,
-          NEXT_PUBLIC_AXIOM_TOKEN: process.env.NEXT_PUBLIC_AXIOM_TOKEN!,
-          NEXT_PUBLIC_AXIOM_DATASET: process.env.NEXT_PUBLIC_AXIOM_DATASET!,
-          AXIOM_ORG_ID: process.env.AXIOM_ORG_ID!,
-          AXIOM_URL: process.env.AXIOM_URL!,
           GOOGLE_PROVIDER_CLIENT_SECRET:
             process.env.GOOGLE_PROVIDER_CLIENT_SECRET!,
-          NEXT_PUBLIC_GOOGLE_PROVIDER_CLIENT_ID:
-            process.env.NEXT_PUBLIC_GOOGLE_PROVIDER_CLIENT_ID!,
+          AXIOM_ORG_ID: process.env.AXIOM_ORG_ID!,
+          AXIOM_URL: process.env.AXIOM_URL!,
         },
       },
+    },
+    customDomain: {
+      domainName:
+        stack.stage === "prod"
+          ? `api.${BASE_CUSTOM_DOMAIN}`
+          : `${stack.stage}-api.${BASE_CUSTOM_DOMAIN}`,
+      hostedZone: BASE_CUSTOM_DOMAIN,
     },
     cors: {
       allowMethods: ["GET", "POST", "OPTIONS"],
@@ -35,19 +47,24 @@ export function SkylarEmailConstructs({ stack }: StackContext) {
     dev: {
       url: "http://localhost:3000",
     },
+    customDomain: {
+      domainName:
+        stack.stage === "prod"
+          ? BASE_CUSTOM_DOMAIN
+          : `${stack.stage}.${BASE_CUSTOM_DOMAIN}`,
+      hostedZone: BASE_CUSTOM_DOMAIN,
+      domainAlias:
+        stack.stage === "prod" ? `www.${BASE_CUSTOM_DOMAIN}` : undefined,
+    },
     warm: 15,
     environment: {
       NEXT_PUBLIC_BACKEND_URL: backend.url,
-      NEXT_PUBLIC_GOOGLE_PROVIDER_CLIENT_ID:
-        process.env.NEXT_PUBLIC_GOOGLE_PROVIDER_CLIENT_ID!,
       NEXT_PUBLIC_POSTHOG_KEY: process.env.NEXT_PUBLIC_POSTHOG_KEY!,
-      NEXT_PUBLIC_AXIOM_TOKEN: process.env.NEXT_PUBLIC_AXIOM_TOKEN!,
-      NEXT_PUBLIC_AXIOM_DATASET: process.env.NEXT_PUBLIC_AXIOM_DATASET!,
     },
   });
 
   stack.addOutputs({
-    BackendUrl: backend.url,
-    FrontendUrl: frontend.url,
+    BackendUrl: backend.customDomainUrl ?? backend.url,
+    FrontendUrl: frontend.customDomainUrl ?? frontend.url,
   });
 }
