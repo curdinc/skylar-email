f5b98d4ace29668fed75d89a74f8e5c22a0bfbb3
ElasticBottle <winstonyeo99@yahoo.com>
2023-12-14 06:51:44 +0000

    fix: don't stop loader on connect provider success

--- apps/frontend/src/app/(emailClient)/onboarding/connect/use-connect-page.ts
+++ apps/frontend/src/app/(emailClient)/onboarding/connect/use-connect-page.ts
@@ -47,8 +47,6 @@ export function useConnectEmailProviderPage() {
         error,
         variables,
       });
-    },
-    onSettled: () => {
       setIsConnectingToProvider(false);
     },
     onSuccess: () => {
