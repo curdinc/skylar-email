48ee1d771a227fd6e428095f758b83906411f505
HansBhatia <h23bhati@uwaterloo.ca>
2023-12-24 23:48:46 +0000

    chore(gmail-api): remove unused inputs

--- packages/gmail-api/index.ts
+++ packages/gmail-api/index.ts
@@ -1,14 +1,5 @@
-export {
-  // batchCreateLabels,
-  batchModifyLabels,
-  batchTrashThreads,
-} from "./src/core-api";
+export { batchModifyLabels, batchTrashThreads } from "./src/core-api";
 
-export {
-  getAccessToken,
-  listLabels,
-  modifyLabels,
-  sendMail,
-} from "./src/core-api";
+export { getAccessToken, listLabels, sendMail } from "./src/core-api";
 export { incrementalSync } from "./src/routines/incremental-sync";
 export { partialSync } from "./src/routines/partial-sync";
