d27c60c0f7ff182dff7252f20de3efdeeca95a03
Winston Yeo <winstonyeo99@yahoo.com>
2023-12-25 17:28:45 +0000

    chore(frontend): remove dup function

--- packages/web-worker-logic/index.ts
+++ packages/web-worker-logic/index.ts
@@ -1,3 +1,4 @@
 export { gmailApiWorker } from "./src/basic-client/gmail-api-worker";
 
 export { gmailBackgroundSyncWorker } from "./src/basic-client/gmail-bg-sync-worker";
+export { convertGmailEmailToClientDbEmail } from "./src/gmail-background-sync/lib/utils";
