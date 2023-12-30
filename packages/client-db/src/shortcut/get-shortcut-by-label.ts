f4cf9d6b5a1944884eaac2737d8526c460e75c24
Winston Yeo <winstonyeo99@yahoo.com>
2023-12-30 02:13:45 +0000

    feat(client-db): Add method to get indiv shortcut info

---
+++ packages/client-db/src/shortcut/get-shortcut-by-label.ts
@@ -0,0 +1,8 @@
+import type { ShortcutIndexType } from "@skylar/parsers-and-types";
+
+import { clientDb } from "../db";
+
+export const getShortcutByLabel = async (shortcutLabel: string) => {
+  return clientDb.shortcut
+    .where("label" satisfies keyof ShortcutIndexType).equals(shortcutLabel).first()
+};
