40290a9bdc9fe4d66d5f34105dc3ae15a523e6e5
Winston Yeo <winstonyeo99@yahoo.com>
2023-12-30 15:36:50 +0000

    chore(frontend): h-12 to min-h-12 for thread subject

--- apps/frontend/src/components/thread-viewer/index.tsx
+++ apps/frontend/src/components/thread-viewer/index.tsx
@@ -23,7 +23,7 @@ export function ThreadViewer() {
 
   return (
     <div className="@container w-full ">
-      <div className="@sm:text-lg @md:text-xl @sm:p-3 @md:p-5 sticky top-0 flex h-12 items-center border-b bg-background p-2 font-heading font-bold tracking-tighter text-foreground">
+      <div className="@sm:text-lg @md:text-xl @sm:p-3 @md:p-5 min-h-12 sticky top-0 flex items-center border-b bg-background p-2 font-heading font-semibold tracking-tighter text-foreground">
         {thread?.[0]?.subject}
       </div>
       <div className="@sm:p-3 @md:p-5 grid gap-3 p-2">{MessageList}</div>
