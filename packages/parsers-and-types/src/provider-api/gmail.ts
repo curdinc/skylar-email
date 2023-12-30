d044e2da6fc97cec9bb2d0bd6b063d319374f967
HansBhatia <h23bhati@uwaterloo.ca>
2023-12-30 16:10:07 +0400

    chore(web-worker-logic): move types to parsers and types

---
+++ packages/parsers-and-types/src/provider-api/gmail.ts
@@ -0,0 +1,33 @@
+import { intersect, object, optional, string } from "valibot";
+
+import { BASE_PROCEDURES_TYPES } from "./base";
+
+export const GMAIL_PROCEDURES_TYPES = {
+  ...BASE_PROCEDURES_TYPES,
+  // overriden types
+  SYNC_PROCEDURES: {
+    incrementalSync: {
+      ...BASE_PROCEDURES_TYPES.SYNC_PROCEDURES.incrementalSync,
+      input: intersect([
+        BASE_PROCEDURES_TYPES.SYNC_PROCEDURES.incrementalSync.input,
+        object({ pageToken: optional(string()) }),
+      ]),
+    },
+    partialSync: {
+      ...BASE_PROCEDURES_TYPES.SYNC_PROCEDURES.partialSync,
+      input: intersect([
+        BASE_PROCEDURES_TYPES.SYNC_PROCEDURES.partialSync.input,
+        object({ startHistoryId: string() }),
+      ]),
+    },
+  },
+  MESSAGE_PROCEDURES: {
+    send: {
+      ...BASE_PROCEDURES_TYPES.MESSAGE_PROCEDURES.send,
+      input: intersect([
+        BASE_PROCEDURES_TYPES.MESSAGE_PROCEDURES.send.input,
+        object({ replyToGmailThreadId: optional(string()) }),
+      ]),
+    },
+  },
+};
